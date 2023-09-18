51096932da93c88b611363bdb9807c51a7279356
Sergio Ly <ly.sergio480@gmail.com>
2023-09-18 14:03:26 -0400

    Set availability zone for support for  instances

--- main.tf
+++ main.tf
@@ -46,6 +46,7 @@ resource "aws_subnet" "subnet" {
   vpc_id = aws_vpc.vpc.id
 
   cidr_block = "10.0.0.0/24"
+  availability_zone = "us-east-1a"
 
   tags = {
     Name = "${var.name}-subnet"
--- var.tf
+++ var.tf
@@ -10,11 +10,14 @@ variable "ports" {
 
 variable "instance_type" {
   type = string
-  default = "c7g.4xlarge"
+  default = "c7gn.xlarge"
 }
 
 variable "ami" {
   type = string
   // first on is arm ubuntu, second is x86 ubuntu
   default = true ? "ami-0a0c8eebcdd6dcbd0" : "ami-053b0d53c279acc90"
+
+  // first is arm amazon linux, second is x86
+  // default = true ? "ami-06f9c0b2ce386dda7" : "ami-04cb4ca688797756f"
 }
