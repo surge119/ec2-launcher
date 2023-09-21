c91bbea6e3564f793a5eb18fa52d790dee771ee6
Sergio <ly.sergio480@gmail.com>
2023-09-20 21:59:07 -0400

    Set file permissions for key.pem

--- var.tf
+++ var.tf
@@ -1,15 +1,15 @@
 variable "name" {
-  type = string
+  type    = string
   default = "ec2_launcher"
 }
 
 variable "ports" {
-  type = list(number)
-  default = [ 22, 8080 ]
+  type    = list(number)
+  default = [22, 8080]
 }
 
 variable "instance_type" {
-  type = string
+  type    = string
   default = "c7gn.xlarge"
 }
 
@@ -17,7 +17,6 @@ variable "ami" {
   type = string
   // first on is arm ubuntu, second is x86 ubuntu
   default = true ? "ami-0a0c8eebcdd6dcbd0" : "ami-053b0d53c279acc90"
-
   // first is arm amazon linux, second is x86
   // default = true ? "ami-06f9c0b2ce386dda7" : "ami-04cb4ca688797756f"
 }
